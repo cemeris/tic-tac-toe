bdb9642ffd4a8f1023fdf11084fd617c405b70e2
cemeris <4vitalik4@gmail.com>
2020-11-25 19:44:04 +0200

    added x o adding logic

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let cells = document.querySelectorAll(".board td");
+let active_player = "player-x";
 
 for (let cell of cells) {
   cell.addEventListener("click", function () {
-    cell.classList.add("player-x");
+    if (
+      !cell.classList.contains("player-x") &&
+      !cell.classList.contains("player-o")
+    ) {
+      cell.classList.add(active_player);
+
+      if (active_player == "player-x") {
+        active_player = "player-o";
+      } else {
+        active_player = "player-x";
+      }
+    }
   });
 }
